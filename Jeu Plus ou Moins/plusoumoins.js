63e009e9207612a528c0e738ca7918577bf8a69c
Samy <35836274+xhackax47@users.noreply.github.com>
2018-03-16 21:20:34 +0100

    Add files via upload

--- Jeu Plus ou Moins/plusoumoins.js
+++ Jeu Plus ou Moins/plusoumoins.js
@@ -1,6 +1,8 @@
-// Création de la fonction prompt
-
-
+/*
+/                                       JEU DU PLUS ET DU MOINS
+/                                           JAVA SCRIPT
+/@author= Samy
+*/
 
 // Création de la fonction nb_aleatoire 
 
@@ -22,30 +24,54 @@ console.log ("L'ordinateur va générer un nombre au hasard" +"\n")
 console.log ("A vous de deviner lequel est-ce..." +"\n")
 console.log ("L'ordinateur vous dira si vous êtes trop haut ou trop bas" +"\n")
 
-// Génération du RandomNumber
+// Génération du nombre aléatoire servant pour la victoire
 
-var nb = nb_aleatoire(1, 100);
+var nbwin = nb_aleatoire(1, 100);
 console.log("L'ordinateur lance le dé.... " +"\n");
 console.log("A vous de deviner maintenant :" +"\n")
-console.log("NOTE DEV:test variable nb" + nb)
+console.log("NOTE DEV:test variable nb = " + nbwin)
 
-// Conditions de victoire ou défaite
+// Déclarer constante de l'invite de commande
 
-var compteCoups = 0;
-var saisie;
+const prompt = require('prompt');
+        
+// Demande à l'utilisateur de choisir un nombre
 
-do
-{   function prompt(saisie) { 
+prompt.start();
+prompt.get(['chiffre'], function (err, result){
 
-        var saisie = prompt("Le nombre à deviner est compris entre 1 et 100.","");
-        compteCoups++;
-        // Message a afficher au prochain tour :
-        if(saisie > nb)
-          console.log ("C'est moins");
-        else
-          console.log("C'est plus");
-}
-}
-while(saisie != nb);
+const chiffre = Number(result.chiffre);
 
-console.log("Bravo, tu as gagne en " + compteCoups + " coups !");
+// Boucle d'appel "Fais la verification tant que la variable saisie est différente de la variable nb"
+
+
+    do
+
+    {
+
+      if (chiffre === nbwin) 
+        {
+        // L'utilisateur à bien répondu
+        console.log("Victoire !!!!! Bien joué le chiffre était : ", nbwin);
+        // Nous demandons a Node de fermer le programme
+        process.exit();
+        }
+
+        else if (chiffre < nbwin) 
+        {
+        console.log("Un peu trop bas, recommence");
+        process.exit();
+        } 
+
+        else 
+        {
+          console.log("Un peu trop haut, recommence");
+          process.exit();
+        }
+      }
+
+    while(chiffre != nbwin); 
+
+  }
+)
+;
